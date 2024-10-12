b940754d62634f6421e1b5e4c0e136b657bc316a
tmba <ut42.nu@gmail.com>
2024-10-12 18:55:12 +0900

    Created MyMusic Project

---
+++ MyMusic/MyMusic/MyMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMusicApp.swift
+//  MyMusic
+//
+//  Created by Takuya Uehara on 2024/10/12.
+//
+
+import SwiftUI
+
+@main
+struct MyMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
