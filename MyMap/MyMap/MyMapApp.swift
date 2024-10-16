053d7fc50ee97eec13dd0512c7327892a4071324
tmba <ut42.nu@gmail.com>
2024-10-16 17:40:35 +0900

    MyMapプロジェクトを作成

---
+++ MyMap/MyMap/MyMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMapApp.swift
+//  MyMap
+//
+//  Created by Takuya Uehara on 2024/10/16.
+//
+
+import SwiftUI
+
+@main
+struct MyMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
